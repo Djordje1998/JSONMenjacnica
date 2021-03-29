3451b5d919c50bdab16ae73b37757d06d9237c4f
Djordje1998 <djordjen7@gmail.com>
2021-03-29 18:13:17 +0200

    Napravljena klasa Transakcija.
    U okviru klase kreirani su atributi izvnornaValuta, krajnjaValuta,
    pocetniIznos, konvertovaniIznos, datumTransakcije i geteri i seteri za
    svaki atribut.

---
+++ src/main/java/rs/ac/bg/fon/ai/JSONMenjacnica/transakcija/Transakcija.java
@@ -0,0 +1,60 @@
+package rs.ac.bg.fon.ai.JSONMenjacnica.transakcija;
+
+import java.util.Date;
+
+public class Transakcija {
+
+	private String izvnornaValuta;
+	private String krajnjaValuta;
+	private double pocetniIznos;
+	private double konvertovaniIznos;
+	private Date datumTransakcije;
+	
+	public String getIzvnornaValuta() {
+		return izvnornaValuta;
+	}
+	
+	public void setIzvnornaValuta(String izvnornaValuta) {
+		this.izvnornaValuta = izvnornaValuta;
+	}
+	
+	public String getKrajnjaValuta() {
+		return krajnjaValuta;
+	}
+	
+	public void setKrajnjaValuta(String krajnjaValuta) {
+		this.krajnjaValuta = krajnjaValuta;
+	}
+	
+	public double getPocetniIznos() {
+		return pocetniIznos;
+	}
+	
+	public void setPocetniIznos(double pocetniIznos) {
+		this.pocetniIznos = pocetniIznos;
+	}
+	
+	public double getKonvertovaniIznos() {
+		return konvertovaniIznos;
+	}
+	
+	public void setKonvertovaniIznos(double konvertovaniIznos) {
+		this.konvertovaniIznos = konvertovaniIznos;
+	}
+	
+	public Date getDatumTransakcije() {
+		return datumTransakcije;
+	}
+	
+	public void setDatumTransakcije(Date datumTransakcije) {
+		this.datumTransakcije = datumTransakcije;
+	}
+	
+	@Override
+	public String toString() {
+		return "Transakcija [izvnornaValuta=" + izvnornaValuta + ", krajnjaValuta=" + krajnjaValuta + ", pocetniIznos="
+				+ pocetniIznos + ", konvertovaniIznos=" + konvertovaniIznos + ", datumTransakcije=" + datumTransakcije
+				+ "]";
+	}
+	
+}
