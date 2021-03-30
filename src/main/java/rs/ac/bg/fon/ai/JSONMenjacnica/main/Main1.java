602e03774e2e1f1a3666a54352aba322f5be16de
Djordje1998 <djordjen7@gmail.com>
2021-03-30 12:48:17 +0200

    Unapredjena klasa Main1

--- src/main/java/rs/ac/bg/fon/ai/JSONMenjacnica/main/Main1.java
+++ src/main/java/rs/ac/bg/fon/ai/JSONMenjacnica/main/Main1.java
@@ -1,14 +1,19 @@
 package rs.ac.bg.fon.ai.JSONMenjacnica.main;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 
 import rs.ac.bg.fon.ai.JSONMenjacnica.transakcija.Transakcija;
 
@@ -19,35 +24,47 @@ public class Main1 {
 	private static final String API_KEY = "2e4baadf5c5ae6ba436f53ae5558107f";
 	private static final String SOURCE = "USD";
 	private static final String CURRENCIES = "CAD";
+	private static final int VALUE = 411;
 	private static final SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 
+	private static Gson gson= new Gson();
+
+	private static double zamenaKursa(double pocetniIznos,String method,String endpoint) throws Exception {
+		double konvertovano=-1;
+		try {
+			URL url = new URL(BASE_URL + "/" + endpoint +"?access_key=" + API_KEY + "&source=" + SOURCE + "&currencies=" + CURRENCIES);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod(method);
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			JsonObject respond = gson.fromJson(in, JsonObject.class);
+			if(respond.get("success").getAsBoolean()) {
+				double kurs = respond.get("quotes").getAsJsonObject().get(SOURCE+CURRENCIES).getAsDouble();
+				konvertovano = kurs * pocetniIznos;
+				return konvertovano;
+			}else {
+				throw new Exception("Can't connect to API");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+	
 	public static void main(String[] args) {
 
 		Transakcija t1 = new Transakcija();
 		t1.setIzvnornaValuta(SOURCE);
 		t1.setKrajnjaValuta(CURRENCIES);
 		t1.setDatumTransakcije(format.format(new Date()));
-		t1.setPocetniIznos(411);
-		double konvertovano = -1;
+		t1.setPocetniIznos(VALUE);
 		
 		try {
-			Gson gson = new Gson(); 
-			URL url = new URL(BASE_URL + "/live?access_key=" + API_KEY + "&source=" + SOURCE + "&currencies=" + CURRENCIES);
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("GET");
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			JsonObject respond = gson.fromJson(in, JsonObject.class);
-			if(respond.get("success").getAsBoolean()) {
-				double kurs = respond.get("quotes").getAsJsonObject().get(SOURCE+CURRENCIES).getAsDouble();
-				konvertovano = kurs * t1.getPocetniIznos();
-				t1.setKonvertovaniIznos(konvertovano);
-			}else {
-				throw new Exception("Can't connect to API");
-			}
+			t1.setKonvertovaniIznos(zamenaKursa(t1.getPocetniIznos(),"GET","live"));
 		} catch (Exception e) {
 			e.printStackTrace();
+			System.out.println("Doslo je do greske");
 		}
-
+		
 		System.out.println(t1);
 		
 	}
